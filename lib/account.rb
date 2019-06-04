4daccc61055ee191190d819b3376756fe256eb68
shektor <44544977+shektor@users.noreply.github.com>
2019-06-04 11:33:47 +0100

    Add account statement
    Basic implementation to pass feature test.

--- lib/account.rb
+++ lib/account.rb
@@ -19,6 +19,14 @@ class Account
     debit(amount)
   end
 
+  def statement
+    string = 'date || credit || debit || balance ||'
+      + '14/01/2012 || || 500.00 || 2500.00'
+      + '13/01/2012 || 2000.00 || || 3000.00'
+      + '10/01/2012 || 1000.00 || || 1000.00'
+    string
+  end
+
   private
 
   def credit(amount)
